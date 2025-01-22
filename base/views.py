105a4d65ea1740a940cad8861c90f741ab76e830
ana-luisa-caixeta <ana.caixeta@estudante.ifb.edu.br>
2025-01-21 21:07:18 -0300

    Correção pós conflito

--- base/views.py
+++ base/views.py
@@ -2,22 +2,23 @@ from django.shortcuts import render
 from .models import Category, Product
 
 # Create your views here.
+categories = Category.objects.all()
+products = Product.objects.all()
+
 def home(request):
-    categories = Category.objects.all()
-    products = Product.objects.all()
     return render(request, "base/home.html", {'categories' : categories, 'products': products})
 
 
 def shopping_cart(request):
-    return render(request, "base/shopping_cart.html")
+    return render(request, "base/shopping_cart.html", {'categories' : categories})
 
 
 def login(request):
-    return render(request, "base/login.html")
+    return render(request, "base/login.html", {'categories' : categories})
 
 
 def cadastro(request):
-    return render(request, "base/register.html")
+    return render(request, "base/register.html", {'categories' : categories})
 
 
 def busca(request):
@@ -27,6 +28,7 @@ def busca(request):
     context = {
         'query': query,
         'results': results,
+        'categories' : categories
     }
 
     return render(request, "base/search.html", context)
